abdec019381cdc1bae4a293a8adf032c651bb0d7
bEinG_RudY <pawansinghfea3@gmail.com>
2024-10-28 22:46:13 +0530

    ...

---
+++ JavaScript/DOM.js
@@ -0,0 +1,14 @@
+// When a web page is loaded, the browser creates a Document Object Model for the Page. 
+window.console.log("hi Pawan");
+
+window.console.dir(Window); // printing the html from the JS
+
+// DOM is a way to acces the html from javaScript
+
+// DOM Manipulation
+console.dir(document.body);
+
+
+let heading = document.getElementById("header"); //get html element using ID, Id's are used using # for styling. it should be unique
+
+console.log(heading);
